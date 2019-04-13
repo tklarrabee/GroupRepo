95351b0559f50a794a38c4368be36662d23e869c
Medric Riley <medricr@gmail.com>
2019-04-13 13:05:25 -0700

    final ajax call testing

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,35 @@
 // VARIABLES
-var article_array;
+
 // FUNCTIONS
+
+$(document).ready(function(){
 
 
 function get_articles(search_term){
-
+    $("#formGroupInput1").on("keyup",function(){
+        if($(this) === "return"){
+            console.log($(this));
+        }        
+    })
     $.ajax({
         url: "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=qLYGpSGjKjv43FYGw3Uvv2prfXcCzCU7",
         method: "GET"
     }).then(function(searchTerm){
         // console.log(searchTerm.response.docs);
+        var article_array;
         article_array = searchTerm.response.docs;
         console.log(article_array);
         
-        for(let i=article_array.length;i>0;i--){
-            var newdiv = $('<a src="'+ article_array[i].web_url +'">');
+        for(let i=article_array.length-1;i>0;i--){
+            var newdiv = $('<div class="card">');
             newdiv.addClass("results");
-            newdiv.append('<h3>'+ article_array[i].headline.main + '</h3>');
-            // console.log(article_array[i].headline.main);
-            newdiv.append('<p>' + article_array[i].snippet + '</p>');
-            // console.log(article_array[i].snippet);
-            // newdiv.attr("data-link",article_array[i].web_url);
-            // console.log(article_array[i].web_url);
+            newdiv.append('<a href="'+article_array[i].web_url+'"><h3 class="card-title">'+ article_array[i].headline.main + '</h3></a>');
+            newdiv.append('<p class="card-body">' + article_array[i].snippet + '</p>');
+            $("#result_card").append(newdiv);
         }
     })
 }
 
 
 get_articles();
+});
